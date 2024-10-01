493f03913d60f9868cdf85c6420df763af5adb3b
ashley <ashnand2000@gmail.com>
2024-10-01 17:05:53 +0530

    Shop-List: 6.UI implementation for Dialog and adding new item in list

--- app/src/main/java/com/project/shoppinglistkotlin/ShoppingList.kt
+++ app/src/main/java/com/project/shoppinglistkotlin/ShoppingList.kt
@@ -2,13 +2,16 @@ package com.project.shoppinglistkotlin
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -26,11 +29,12 @@ data class ShoppingItems(val id:Int,
                          val quantity:Int,
                          var editing:Boolean = false)
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ShoppingListApp(){
     var sItems by remember{ mutableStateOf( listOf<ShoppingItems>()) }
     var showDialog by remember{ mutableStateOf( false ) }
+    var itemName by remember{ mutableStateOf( "" ) }
+    var itemQuantity by remember{ mutableStateOf( "1" ) }
 
     Column(
         modifier = Modifier.fillMaxSize(),
@@ -56,8 +60,58 @@ fun ShoppingListApp(){
     }
     
     if (showDialog) {
-        AlertDialog(onDismissRequest = { showDialog = false }) {
-            Text(text = "Hey Developers!")
-        }
+        AlertDialog(onDismissRequest = { /*TODO*/ },
+            confirmButton = {
+                            Row(modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(8.dp),
+                                horizontalArrangement = Arrangement.SpaceBetween) {
+
+                                Button(onClick = {
+                                    // storing data in list
+                                    if (itemName.isNotEmpty()){
+                                        val newItem = ShoppingItems(
+                                            id = sItems.size + 1,
+                                            name = itemName,
+                                            quantity = itemQuantity.toInt(),
+                                        )
+
+                                        sItems = sItems + newItem
+                                        showDialog = false
+                                        itemName = ""
+                                        itemQuantity = "1"
+                                    }
+                                }) {
+                                    Text(text = "Add")
+                                }
+
+                                Button(onClick = { showDialog = false }) {
+                                    Text(text = "Cancel")
+                                }
+
+                            }
+            },
+            title = { Text(text = "Add Shopping Item") },
+            text = {
+                Column {
+                    OutlinedTextField(value = itemName,
+                        onValueChange = {itemName = it},
+                        singleLine = true,
+                        label = { Text(text = "Enter Item")},
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(8.dp))
+
+                    OutlinedTextField(value = itemQuantity,
+                        onValueChange = {itemQuantity = it},
+                        singleLine = true,
+                        label = { Text(text = "Enter Quantity")},
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(8.dp))
+
+                }
+            }
+            )
     }
 }
