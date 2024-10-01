f3cd263eb24cb752cc8511fa5c6eb6de479ebc83
ashley <ashnand2000@gmail.com>
2024-10-01 16:50:30 +0530

    Shop-List: 5.Creating Alert Dialog for Adding items

--- app/src/main/java/com/project/shoppinglistkotlin/ShoppingList.kt
+++ app/src/main/java/com/project/shoppinglistkotlin/ShoppingList.kt
@@ -6,7 +6,9 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -24,16 +26,18 @@ data class ShoppingItems(val id:Int,
                          val quantity:Int,
                          var editing:Boolean = false)
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ShoppingListApp(){
     var sItems by remember{ mutableStateOf( listOf<ShoppingItems>()) }
+    var showDialog by remember{ mutableStateOf( false ) }
 
     Column(
         modifier = Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Center
     ) {
         Button(
-            onClick = { /*TODO*/ },
+            onClick = { showDialog = true },
             modifier = Modifier
                 .align(Alignment.CenterHorizontally)
                 .padding(0.dp, 25.dp, 0.dp, 0.dp)) {
@@ -48,6 +52,12 @@ fun ShoppingListApp(){
             items(sItems){
 
             }
+        }
+    }
+    
+    if (showDialog) {
+        AlertDialog(onDismissRequest = { showDialog = false }) {
+            Text(text = "Hey Developers!")
         }
     }
 }
