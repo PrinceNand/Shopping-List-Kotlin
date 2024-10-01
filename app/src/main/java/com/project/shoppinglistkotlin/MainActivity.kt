d406a8d1628cd039f6310eba71c1cccb9ef413d0
ashley <ashnand2000@gmail.com>
2024-10-01 14:06:35 +0530

    Shop-List: 3.Creating Lazy Column and Data Class(Shopping Items)

--- app/src/main/java/com/project/shoppinglistkotlin/MainActivity.kt
+++ app/src/main/java/com/project/shoppinglistkotlin/MainActivity.kt
@@ -9,16 +9,23 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.project.shoppinglistkotlin.ui.theme.ShoppingListKotlinTheme
 
 class MainActivity : ComponentActivity() {
@@ -31,11 +38,24 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     verticalArrangement = Arrangement.Center
                 ) {
-                    val context = LocalContext.current
+//                    val context = LocalContext.current
+                    var sItems by remember{ mutableStateOf( listOf<ShoppingItems>()) }
                     Button(
-                        onClick = { Toast.makeText(context, "Add Items", Toast.LENGTH_SHORT).show() },
-                        modifier = Modifier.align(Alignment.CenterHorizontally)) {
+                        onClick = {
+//                            Toast.makeText(context, "Add Items", Toast.LENGTH_SHORT).show()
+                        },
+                        modifier = Modifier.align(Alignment.CenterHorizontally).padding(0.dp,25.dp,0.dp,0.dp)) {
                         Text(text = "Add Items")
+                    }
+
+                    LazyColumn(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(16.dp),
+                    ) {
+                        items(sItems){
+
+                        }
                     }
                 }
             }
@@ -43,3 +63,5 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+
+data class ShoppingItems(val id:Int, var name:String, val quantity:Int, var editing:Boolean = false)
